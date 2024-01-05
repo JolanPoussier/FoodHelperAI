2acafc2adc66d325ab65e156621964df1f51eeb9
JolanPoussier <jojopoussier@gmail.com>
2024-01-05 10:23:43 +0100

    feat: connection with api, home page, stream version

---
+++ app/api/foodrequest/route.ts
@@ -0,0 +1,26 @@
+import { openai } from "@/app/src/lib/openai";
+import { NextResponse } from "next/server";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+export const POST = async (req: Request) => {
+  const { dataForm } = await req.json();
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages: [
+      {
+        role: "assistant",
+        content: `Ecris en français une recette étape par étape, avec étapes numérotées et quantités et temps bien précis.`,
+      },
+      {
+        role: "user",
+        content: dataForm,
+      },
+    ],
+  });
+
+  const stream = OpenAIStream(response);
+
+  return new StreamingTextResponse(stream);
+};
